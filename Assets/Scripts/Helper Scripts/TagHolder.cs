d718d83e89149ce230f886356d7b9858b64bdf95
Nithin Rauj <nithindrake@gmail.com>
2019-04-18 19:00:34 +0530

    Created basic player movement in PlayerMovementController
    Created basic player movement in PlayerMovementController

---
+++ Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis
+{
+    public static string HORIZONTAL="Horizontal";
+    public static string VERTICAL="Vertical";
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+private CharacterController characterController;
+private Vector3 moveDirection;
+public float speed=5f;
+private float gravity=20f;
+public float jumpForce=10f;
+private float verticalVelocity;
+
+
+void Awake()
+{
+    characterController=GetComponent<CharacterController>();
+}
+
+void Update()
+{
+    MovePlayer();
+}
+
+void MovePlayer()
+{
+    moveDirection=new Vector3(Input.GetAxis(Axis.HORIZONTAL),0,Input.GetAxis(Axis.VERTICAL));
+    moveDirection=transform.TransformDirection(moveDirection);
+    moveDirection*=speed*Time.deltaTime;
+    characterController.Move(moveDirection);
+}
+}
