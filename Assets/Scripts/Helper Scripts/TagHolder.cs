262fe5c65dbf8743ed7c55320134cf4d42329a71
Nithin Rauj <nithindrake@gmail.com>
2019-05-07 18:09:47 +0530

    Created WeaponHandler script for induvidual weapons
    Created WeaponHandler script for induvidual weapons

--- Assets/Scripts/Helper Scripts/TagHolder.cs
+++ Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Axis :MonoBehaviour
 {
-    public static string HORIZONTAL="Horizontal";
-    public static string VERTICAL="Vertical";
+    public const string HORIZONTAL="Horizontal";
+    public const string VERTICAL="Vertical";
     
 }
 
 public class MouseAxis:MonoBehaviour
 {
-    public static string MOUSEX="Mouse X";
-    public static string MOUSEY="Mouse Y";
+    public const string MOUSEX="Mouse X";
+    public const string MOUSEY="Mouse Y";
+}
+
+public class AnimationTags : MonoBehaviour
+{
+    public const string ZOOM_IN="ZoomIn";
+    public const string ZOOM_OUT="ZoomOut";
+    public const string ATTACK_PARAMETER="Attack";
+    public const string AIM_TRIGGER="Aim";
+    public const string ATTACK_TRIGGER="Attack";
+    public const string WALK_TRIGGER="Walk";
+    public const string RUN_TRIGGER="Run";
+    public const string DEAD_TRIGGER="Dead"; 
+}
+
+public class Tags:MonoBehaviour
+{
+    public const string LOOK_ROOT="Look Root";
+    public const string ZOOM_CAMERA="FP Camera";
+    public const string CROSSHAIR="Crosshair";
+    public const string AXE_TAG="Axe";
+    public const string PLAYER_TAG="Player";
+    public const string ENEMY_TAG="Enemy";
 }
---
+++ Assets/Scripts/WeaponHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim {NONE,SELF_AIM,AIM}
+public enum WeaponFireType {SINGLE,AUTOMATIC}
+public enum WeaponBulletType {BULLET,ARROW,NONE}
+public class WeaponHandler : MonoBehaviour {
+    private Animator anim;
+    [SerializeField]
+    private AudioSource firing_Sound,reload_Sound;
+    public WeaponFireType fire_Type;
+    public WeaponAim aim_Type;
+    public WeaponBulletType bullet_Type;
+    [SerializeField]
+    private GameObject muzzle_Flash;
+    private GameObject attack_Point;
+
+    void Awake()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    public void TriggerAttackAnimation()
+    {
+        anim.SetTrigger(AnimationTags.ATTACK_PARAMETER);
+    }
+
+    public void Aim(bool canAim)
+    {
+        anim.SetBool(AnimationTags.AIM_TRIGGER,canAim);
+    }
+
+    public void PlayFiringSound()
+    {
+        firing_Sound.Play();
+    }
+    public void PlayReloadSound()
+    {
+        reload_Sound.Play();
+    }
+
+    public void TurnOnMuzzleFlash()
+    {
+        muzzle_Flash.SetActive(true);
+    }
+    public void TurnOffMuzzleFlash()
+    {
+        muzzle_Flash.SetActive(false);
+    }
+    
+    public void TurnOnAttackPoint()
+    {
+        attack_Point.SetActive(true);
+    }
+	public void TurnOffAttackPoint()
+    {
+        if(attack_Point.activeInHierarchy)
+            attack_Point.SetActive(false);
+    }
+}
