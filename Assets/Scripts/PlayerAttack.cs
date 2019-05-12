e3b26a2f257610e3e76de57f8c189a26b6bba852
Nithin Rauj <nithindrake@gmail.com>
2019-05-12 18:57:17 +0530

    Created PlayerAttack script for simulating shooting effect in weapons
    Created PlayerAttack script for simulating shooting effect in weapons

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,5 +4,50 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
 
-	
+	private WeaponManager weapon_Manager;
+    private float rateOfFire=15f;
+    private float nextTimeToFire;
+    private float damage=20f;
+
+    void Start()
+    {
+        weapon_Manager=GetComponent<WeaponManager>();
+    }
+
+    void Update()
+    {
+        PlayerShoot();
+    }
+
+    void PlayerShoot()
+    {
+        //if current weapon is assault rifle
+         if(weapon_Manager.ReturnCurrentWeapon().fire_Type==WeaponFireType.AUTOMATIC)
+        {
+            if(Input.GetMouseButton(0) && Time.time>nextTimeToFire)
+            {
+                 nextTimeToFire=Time.time+1/rateOfFire;
+                 weapon_Manager.ReturnCurrentWeapon().TriggerAttackAnimation();
+            }
+        }
+
+        //if current weapon has single fire mode
+        else
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                //if current weapon is axe
+                if(weapon_Manager.ReturnCurrentWeapon().tag==Tags.AXE_TAG)
+                {
+                    weapon_Manager.ReturnCurrentWeapon().TriggerAttackAnimation();
+                }
+                
+                else
+                {
+                    weapon_Manager.ReturnCurrentWeapon().TriggerAttackAnimation();
+                    //BulletFired();
+                }
+            }
+        }
+    }
 }
