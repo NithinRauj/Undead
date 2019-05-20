25e28382ae323f59ab116dd6a892d6d657b2fede
Nithin Rauj <nithindrake@gmail.com>
2019-05-20 21:36:09 +0530

    Implemented audio  for player and muzzle flashes for player weapons
    Implemented audio  for player and muzzle flashes for player weapons

---
+++ Assets/Scripts/AxeAudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeAudioManager : MonoBehaviour {
+
+public AudioSource audio_Source;
+public AudioClip[] audio_Clips;
+
+
+void PlayAxeSounds()
+{
+    audio_Source.clip=audio_Clips[Random.Range(0,audio_Clips.Length)];
+    audio_Source.Play();
+}
+	
+}
