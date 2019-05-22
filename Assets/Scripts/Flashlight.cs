921db8b4429bd5a4a13425809e411b6b88077e11
Nithin Rauj <nithindrake@gmail.com>
2019-05-22 17:01:29 +0530

    Added another zombie to the game and created flashlight for player
    Added another zombie to the game and created flashlight for player

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+private AudioSource audio_Source;
+private Light flashlight;
+private bool isLightOn;
+void Awake()
+{
+    flashlight=GetComponent<Light>();
+    audio_Source=GetComponent<AudioSource>();
+}
+
+void Start()
+{
+    flashlight.enabled=true;
+    audio_Source.Play();
+    isLightOn=true;
+}
+void Update()
+{
+    ToggleFlashlight();
+}
+
+void ToggleFlashlight()
+{
+    if(Input.GetKeyDown(KeyCode.F) && !isLightOn)
+    {
+        flashlight.enabled=true;
+        audio_Source.Play();
+        isLightOn=true;
+    }
+    else if(Input.GetKeyDown(KeyCode.F) && isLightOn)
+    {
+        flashlight.enabled=false;
+        audio_Source.Play();
+        isLightOn=false;
+    }
+}
+	
+}
