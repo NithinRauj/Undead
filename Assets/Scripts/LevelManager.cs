268a9c8aa7f718bcb297ac911fdc26c042a3aeb0
Nithin Rauj <nithindrake@gmail.com>
2019-05-31 18:20:32 +0530

    Created additional scenes for game and imported military humvee model
    Created additional scenes for game and imported military humvee model

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour {
+
+private int game_Scene_Index=1;
+private int start_Scene_Index=0;
+
+public void LoadStartScene()
+{
+    SceneManager.LoadScene(start_Scene_Index); 
+}
+public void LoadGameScene()
+{
+    SceneManager.LoadScene(game_Scene_Index);
+}
+
+public void QuitGame()
+{
+    Application.Quit();
+}
+	
+}
